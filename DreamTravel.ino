fb5628cda4aa450bd18b388cce2a963dcdbbfc72
daitianqi <daitianqi@jd.com>
2019-01-28 18:32:47 +0800

    架构

--- DreamTravel.ino
+++ DreamTravel.ino
@@ -23,10 +23,10 @@ MPU6050 mpu;
 bool blinkState = false;
 
 // MPU control/status vars
-bool dmpReady = false;  // set true if DMP init was successful
+bool dmpReady = true;  // set true if DMP init was successful
 uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
 uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes) mpu产生的数据大小
 uint16_t fifoCount;     // count of all bytes currently in FIFO
 uint8_t fifoBuffer[64]; // FIFO storage buffer
 
@@ -68,6 +68,9 @@ void setup() {
         Fastwire::setup(400, true);
     #endif
 
+    // TODO 加入连接协议
+
+
     //设置所有的mpu引脚为输出引脚
     for(int i = 0; i<MPU_NUM; i++){
         pinMode(mpuPins[i], OUTPUT); 
@@ -86,30 +89,20 @@ void setup() {
     for(int i = 0; i<MPU_NUM; i++){
         // 开启mpu
         digitalWrite(mpuPins[i], LOW); 
+        // mpu sample rate  200Hz
         mpu.initialize();
         Serial.print(mpuPins[i]);
         Serial.print("--")
-        Serial.println(mpu.testConnection() ? F("connection successful") : F("connection failed"));
+        Serial.println(mpu.testConnection() ? F("cs") : F("cf"));
         // 关闭mpu
         digitalWrite(mpuPins[i], HIGH); 
     }
     
-    // pinMode(INTERRUPT_PIN, INPUT);
-
-    // verify connection
-    // Serial.println(F("Testing device connections..."));
-    // Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
-
-    // wait for ready
-    // TODO 改成连接协议
-    // Serial.println(F("\nSend any character to begin DMP programming and demo: "));
-    // while (Serial.available() && Serial.read()); // empty buffer
-    // while (!Serial.available());                 // wait for data
-    // while (Serial.available() && Serial.read()); // empty buffer again
-
     // load and configure the DMP
     Serial.println(F("Initializing DMP..."));
     for(int i = 0; i<MPU_NUM; i++){
+        // 开启mpu
+        digitalWrite(mpuPins[i], LOW); 
         devStatus = mpu.dmpInitialize();
         // supply your own gyro offsets here, scaled for min sensitivity
         mpu.setXGyroOffset(220);
@@ -124,19 +117,10 @@ void setup() {
         if (devStatus == 0) {
             // turn on the DMP, now that it's ready
             Serial.println(F("Enabling DMP..."));
+            Serial.print(i);
             mpu.setDMPEnabled(true);
-
-            // // enable Arduino interrupt detection
-            // Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
-            // Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
-            // Serial.println(F(")..."));
-            // attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
             mpuIntStatus = mpu.getIntStatus();
-
-            // set our DMP Ready flag so the main loop() function knows it's okay to use it
-            // Serial.println(F("DMP ready! Waiting for first interrupt..."));
-            dmpReady = true;
-
+            dmpReady = dmpReady && true;
             // get expected DMP packet size for later comparison
             packetSize = mpu.dmpGetFIFOPacketSize();
         } else {
@@ -144,15 +128,15 @@ void setup() {
             // 1 = initial memory load failed
             // 2 = DMP configuration updates failed
             // (if it's going to break, usually the code will be 1)
+            dmpReady = dmpReady && false;
+            Serial.print(i);
             Serial.print(F("DMP Initialization failed (code "));
             Serial.print(devStatus);
             Serial.println(F(")"));
         }
-
+        // 关闭mpu
+        digitalWrite(mpuPins[i], HIGH);
     }
-
-    // configure LED for output
-    pinMode(LED_PIN, OUTPUT);
 }
 
 
@@ -165,136 +149,63 @@ void loop() {
     // if programming failed, don't try to do anything
     if (!dmpReady) return;
 
-    // wait for MPU interrupt or extra packet(s) available
-    while (!mpuInterrupt && fifoCount < packetSize) {
-        // 批量获取数据
-        if (mpuInterrupt && fifoCount < packetSize) {
-          // try to get out of the infinite loop 
-          fifoCount = mpu.getFIFOCount();
-        }  
-        // other program behavior stuff here
-        // .
-        // .
-        // .
-        // if you are really paranoid you can frequently test in between other
-        // stuff to see if mpuInterrupt is true, and if so, "break;" from the
-        // while() loop to immediately process the MPU data
-        // .
-        // .
-        // .
- 
-    }
+    for(int i = 0; i<MPU_NUM; i++){
+        // 开启mpu
+        digitalWrite(mpuPins[i], LOW); 
 
-    // reset interrupt flag and get INT_STATUS byte
-    mpuInterrupt = false;
-    mpuIntStatus = mpu.getIntStatus();
-
-    // get current FIFO count
-    fifoCount = mpu.getFIFOCount();
-
-    // check for overflow (this should never happen unless our code is too inefficient)
-    if ((mpuIntStatus & _BV(MPU6050_INTERRUPT_FIFO_OFLOW_BIT)) || fifoCount >= 1024) {
-        // reset so we can continue cleanly
-        mpu.resetFIFO();
         fifoCount = mpu.getFIFOCount();
-        Serial.println(F("FIFO overflow!"));
 
-    // otherwise, check for DMP data ready interrupt (this should happen frequently)
-    } else if (mpuIntStatus & _BV(MPU6050_INTERRUPT_DMP_INT_BIT)) {
-        // wait for correct available data length, should be a VERY short wait
-        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+        if(fifoCount < packetSize){
+            // TODO 发送上一次的数据
 
-        // read a packet from FIFO
-        mpu.getFIFOBytes(fifoBuffer, packetSize);
+            digitalWrite(mpuPins[i], HIGH); 
+        }
+
+        mpuIntStatus = mpu.getIntStatus();
+        // check for overflow (this should never happen unless our code is too inefficient)
+        if ((mpuIntStatus & _BV(MPU6050_INTERRUPT_FIFO_OFLOW_BIT)) || fifoCount >= 1024) {
+            // reset so we can continue cleanly
+            mpu.resetFIFO();
+            fifoCount = mpu.getFIFOCount();
+            Serial.println(F("FIFO overflow!"));
+
+        // otherwise, check for DMP data ready interrupt (this should happen frequently)
+        } else if (mpuIntStatus & _BV(MPU6050_INTERRUPT_DMP_INT_BIT)) {
+            // wait for correct available data length, should be a VERY short wait
+            while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+           
+            
+
+            // 读取最新数据
+            while(fifoCount / packetSize > 0){
+                // read a packet from FIFO
+                mpu.getFIFOBytes(fifoBuffer, packetSize);
+
+                // track FIFO count here in case there is > 1 packet available
+                // (this lets us immediately read more without waiting for an interrupt)
+                fifoCount -= packetSize;
+
+                mpu.dmpGetQuaternion(&q, fifoBuffer);
+                Serial.print("quat\t");
+                Serial.print(q.w);
+                Serial.print("\t");
+                Serial.print(q.x);
+                Serial.print("\t");
+                Serial.print(q.y);
+                Serial.print("\t");
+                Serial.println(q.z);
+            }
+            
+            
         
-        // track FIFO count here in case there is > 1 packet available
-        // (this lets us immediately read more without waiting for an interrupt)
-        fifoCount -= packetSize;
+        
+        }
 
-        #ifdef OUTPUT_READABLE_QUATERNION
-            // display quaternion values in easy matrix form: w x y z
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            Serial.print("quat\t");
-            Serial.print(q.w);
-            Serial.print("\t");
-            Serial.print(q.x);
-            Serial.print("\t");
-            Serial.print(q.y);
-            Serial.print("\t");
-            Serial.println(q.z);
-        #endif
-
-        #ifdef OUTPUT_READABLE_EULER
-            // display Euler angles in degrees
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            mpu.dmpGetEuler(euler, &q);
-            Serial.print("euler\t");
-            Serial.print(euler[0] * 180/M_PI);
-            Serial.print("\t");
-            Serial.print(euler[1] * 180/M_PI);
-            Serial.print("\t");
-            Serial.println(euler[2] * 180/M_PI);
-        #endif
-
-        #ifdef OUTPUT_READABLE_YAWPITCHROLL
-            // display Euler angles in degrees
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            mpu.dmpGetGravity(&gravity, &q);
-            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-            Serial.print("ypr\t");
-            Serial.print(ypr[0] * 180/M_PI);
-            Serial.print("\t");
-            Serial.print(ypr[1] * 180/M_PI);
-            Serial.print("\t");
-            Serial.println(ypr[2] * 180/M_PI);
-        #endif
-
-        #ifdef OUTPUT_READABLE_REALACCEL
-            // display real acceleration, adjusted to remove gravity
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            mpu.dmpGetAccel(&aa, fifoBuffer);
-            mpu.dmpGetGravity(&gravity, &q);
-            mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-            Serial.print("areal\t");
-            Serial.print(aaReal.x);
-            Serial.print("\t");
-            Serial.print(aaReal.y);
-            Serial.print("\t");
-            Serial.println(aaReal.z);
-        #endif
-
-        #ifdef OUTPUT_READABLE_WORLDACCEL
-            // display initial world-frame acceleration, adjusted to remove gravity
-            // and rotated based on known orientation from quaternion
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            mpu.dmpGetAccel(&aa, fifoBuffer);
-            mpu.dmpGetGravity(&gravity, &q);
-            mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-            mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
-            Serial.print("aworld\t");
-            Serial.print(aaWorld.x);
-            Serial.print("\t");
-            Serial.print(aaWorld.y);
-            Serial.print("\t");
-            Serial.println(aaWorld.z);
-        #endif
-    
-        #ifdef OUTPUT_TEAPOT
-            // display quaternion values in InvenSense Teapot demo format:
-            teapotPacket[2] = fifoBuffer[0];
-            teapotPacket[3] = fifoBuffer[1];
-            teapotPacket[4] = fifoBuffer[4];
-            teapotPacket[5] = fifoBuffer[5];
-            teapotPacket[6] = fifoBuffer[8];
-            teapotPacket[7] = fifoBuffer[9];
-            teapotPacket[8] = fifoBuffer[12];
-            teapotPacket[9] = fifoBuffer[13];
-            Serial.write(teapotPacket, 14);
-            teapotPacket[11]++; // packetCount, loops at 0xFF on purpose
-        #endif
+        // 关闭mpu
+        digitalWrite(mpuPins[i], HIGH); 
+    }
 
         // blink LED to indicate activity
-        blinkState = !blinkState;
-        digitalWrite(LED_PIN, blinkState);
-    }
+        // blinkState = !blinkState;
+        // digitalWrite(LED_PIN, blinkState);
 }
