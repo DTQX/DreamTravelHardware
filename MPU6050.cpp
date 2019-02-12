aaeb9cc5b9b31cfd4f2b1a54965058bec8879de8
daitianqi <daitianqi@jd.com>
2019-02-12 16:44:19 +0800

    dmp采样率修改为100hz

--- MPU6050.cpp
+++ MPU6050.cpp
@@ -3280,7 +3280,7 @@ uint8_t MPU6050::dmpInitialize() {
             setIntEnabled(1<<MPU6050_INTERRUPT_FIFO_OFLOW_BIT|1<<MPU6050_INTERRUPT_DMP_INT_BIT);
 
             DEBUG_PRINTLN(F("Setting sample rate to 200Hz..."));
-            setRate(4); // 1khz / (1 + 4) = 200 Hz
+            setRate(9); // 1khz / (1 + 4) = 200 Hz
 
             DEBUG_PRINTLN(F("Setting external frame sync to TEMP_OUT_L[0]..."));
             setExternalFrameSync(MPU6050_EXT_SYNC_TEMP_OUT_L);
